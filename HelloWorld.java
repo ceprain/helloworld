6006f0f7e0a2e1eb6580b9ceb83a27927f377e8d
rain <rain@rain-PC>
2019-01-04 15:38:27 +0800

    test replace

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,6 @@
 public class HelloWorld {
 
   public static void main(String[] args){
-	    System.out.println("hello world1");
-	    System.out.println("hello world2");
-	    System.out.println("hello world3");
+    System.out.println("hello world");
   }
 }
